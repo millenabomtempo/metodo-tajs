9577231d9b004d8a4b89d279d09e0f3eb4d36492
Millena Bomtempo <millena.bomtempo@gmail.com>
2024-02-01 20:01:09 -0300

    melhorias de código

--- modulo02-setup/aula01-setup/test/tamplate.test.js
+++ modulo02-setup/aula01-setup/test/tamplate.test.js
@@ -1,9 +1,9 @@
-import { it } from "@jest/globals"
+import { it, expect } from "@jest/globals"
 
 function sum(a, b) {
   return a + b
 }
 
-it("sums ttwo values", () => {
+it("sums two values", () => {
   expect(sum(2, 3)).toBe(5)
 })
